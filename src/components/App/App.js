91212ba294df02e3be21fbd8715b93f6a63773de
janekox <60790158+janekox@users.noreply.github.com>
2020-02-18 22:48:41 +0000

    I have added Cards and Pictures to it

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Col, Row} from "react-bootstrap";
+import {Card, CardDeck, Col, Row} from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -11,9 +11,9 @@ import Button from "react-bootstrap/Button";
 function App() {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <div className="container">
-                    <Navbar.Brand href="#home">adsys-frontend</Navbar.Brand>
+                    <Navbar.Brand href="#home">Adverter</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
@@ -38,7 +38,56 @@ function App() {
             <main role="main">
                 <div className="container">
                     <Row className="pt-3 mt-3">
-                        <Col>Hello world</Col>
+                        <CardDeck>
+                            <Card>
+
+                                <Card.Body>
+                                    <Card.Title>Advertisement 1</Card.Title>
+                                    <Card.Img variant="top"  src="https://images.internetstores.de/products//853468/02/bb4d43/NS_Bikes_Zircus_24__Kinder_blue[640x480].jpg?forceSize=true&forceAspectRatio=true&useTrim=true"></Card.Img>
+                                    <Card.Text>
+                                        this is an amazing bike
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+
+                                <Card.Body>
+
+                                    <Card.Title>Advertisement 2</Card.Title>
+                                    <Card.Img variant="top"  src="https://www.elfadistrelec.pl/Web/WebShopImages/landscape_large/2-/01/30110722-01.jpg"></Card.Img>
+                                    <Card.Text>
+                                        This card has supporting text below as a natural lead-in to additional
+                                        content.{' '}
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                </Card.Footer>
+                            </Card>
+                            <Card>
+
+                                <Card.Body>
+                                    <Card.Title>Advertisement 3</Card.Title>
+                                <Card.Img variant="top"  src=""></Card.Img>
+                                    <Card.Text>
+                                        This is a wider card with supporting text below as a natural lead-in to
+                                        additional content. This card has even longer content than the first to
+                                        show that equal height action.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                </Card.Footer>
+                            </Card>
+                        </CardDeck>
+
+
+
+
+
                     </Row>
                 </div>
             </main>
